d5de03b1eb55719b4e5cae9a87bf507337af8dc3
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-22 21:05:35 -0700

    update logic and missing header

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 19:32:48 by seli              #+#    #+#             */
-/*   Updated: 2018/09/22 20:22:00 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/22 20:58:47 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@ void    rush_with_custum_char(int width, int height, char output[6]);
 
 void    rush(int width, int height)
 {
-    char output[6];
+    char output[7];
 
     output[0] = 'o';
     output[1] = 'o';
     output[2] = 'o';
     output[3] = 'o';
-    output[5] = '-';
-    output[6] = '|';
+    output[4] = '-';
+    output[5] = '|';
     rush_with_custum_char(width, height, output);
 }
 
-void    rush_with_custum_char(int width, int height, char output[6])
+void    rush_with_custum_char(int width, int height, char output[7])
 {
     int w;
     int h;
 
-    w = 0;
     h = 0;
     while (h < height)
     {
+        w = 0;
         while (w < width)
         {
             if (h == 0 && w == 0)
@@ -47,10 +47,13 @@ void    rush_with_custum_char(int width, int height, char output[6])
                 ft_putchar(output[3]);
             else if (h == 0 || h == height -1)
                 ft_putchar(output[4]);
-            else
+            else if (w == 0 || w == width - 1)
                 ft_putchar(output[5]);
+            else
+                ft_putchar(' ');
             w++;
         }
+        ft_putchar('\n');
         h++;
     }
 }
--- ex01/rush01.c
+++ ex01/rush01.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 19:32:48 by seli              #+#    #+#             */
-/*   Updated: 2018/09/22 20:21:42 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/22 20:59:03 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@ void    rush_with_custum_char(int width, int height, char output[6]);
 
 void    rush(int width, int height)
 {
-    char output[6];
+    char output[7];
 
     output[0] = '/';
     output[1] = '\\';
     output[2] = '\\';
     output[3] = '/';
+    output[4] = '*';
     output[5] = '*';
-    output[6] = '*';
     rush_with_custum_char(width, height, output);
 }
 
-void    rush_with_custum_char(int width, int height, char output[6])
+void    rush_with_custum_char(int width, int height, char output[7])
 {
     int w;
     int h;
 
-    w = 0;
     h = 0;
     while (h < height)
     {
+        w = 0;
         while (w < width)
         {
             if (h == 0 && w == 0)
@@ -47,10 +47,13 @@ void    rush_with_custum_char(int width, int height, char output[6])
                 ft_putchar(output[3]);
             else if (h == 0 || h == height -1)
                 ft_putchar(output[4]);
-            else
+            else if (w == 0 || w == width - 1)
                 ft_putchar(output[5]);
+            else
+                ft_putchar(' ');
             w++;
         }
+        ft_putchar('\n');
         h++;
     }
 }
